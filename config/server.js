7833c7feb339e7ec3be7fabe67353076eef23763
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-09-27 16:24:08 -0300

    initial files

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f41955b45bfd098d3b860c8eb9841d64'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '517dd815-c5ba-435c-9f51-fa76ec0c60f0'
+};
