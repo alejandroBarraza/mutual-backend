b6a23d25764e1b165dab76d3c56742cc3650dab1
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-12-06 15:39:25 -0300

    error 500

--- config/plugins.js
+++ config/plugins.js
@@ -1,33 +1,39 @@
-module.exports = ({ env }) => ({
-  upload: {
-    provider: "cloudinary",
-    providerOptions: {
-      cloud_name: env("CLOUDINARY_NAME"),
-      api_key: env("CLOUDINARY_KEY"),
-      api_secret: env("CLOUDINARY_SECRET"),
-    },
-    actionOptions: {
-      upload: {},
-      delete: {},
-    },
-  },
-  email: {
-    provider: "sendgrid",
-    providerOptions: {
-      apiKey: env("SENDGRID_API_KEY"),
-    },
-    settings: {
-      defaultFrom: env("SENDGRID_DEFAULT_FROM"),
-      defaultReplyTo: env("SENDGRID_DEFAULT_REPLY_TO"),
-    },
-  },
-  graphql: {
-    endpoint: "/graphql",
-    shadowCRUD: true,
-    playgroundAlways: true,
-    depthLimit: 100,
-    apolloServer: {
-      tracing: false,
-    },
-  },
-});
+module.exports = ({ env }) => {
+  if(env("NODE_ENV") === "production"){
+    return{
+      upload: {
+        provider: "cloudinary",
+        providerOptions: {
+          cloud_name: env("CLOUDINARY_NAME"),
+          api_key: env("CLOUDINARY_KEY"),
+          api_secret: env("CLOUDINARY_SECRET"),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+      email: {
+        provider: "sendgrid",
+        providerOptions: {
+          apiKey: env("SENDGRID_API_KEY"),
+        },
+        settings: {
+          defaultFrom: env("SENDGRID_DEFAULT_FROM"),
+          defaultReplyTo: env("SENDGRID_DEFAULT_REPLY_TO"),
+        },
+      },
+      graphql: {
+        endpoint: "/graphql",
+        shadowCRUD: true,
+        playgroundAlways: true,
+        depthLimit: 100,
+        apolloServer: {
+          tracing: false,
+        },
+      },
+
+    }
+  }
+  return{} 
+};
