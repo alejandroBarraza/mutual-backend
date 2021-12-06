01671d76ce8cecf5a80cd89735dc4db8ace27363
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-12-06 15:30:33 -0300

    fix-error

--- config/plugins.js
+++ config/plugins.js
@@ -1,71 +1,33 @@
-module.exports = ({ env }) => {
-  if(env('NODE_ENV') === 'production') {
-    return{
-      upload: {
-        provider: "cloudinary",
-        providerOptions: {
-          cloud_name: env("CLOUDINARY_NAME"),
-          api_key: env("CLOUDINARY_KEY"),
-          api_secret: env("CLOUDINARY_SECRET"),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
-      },
-      email: {
-        provider: "sendgrid",
-        providerOptions: {
-          apiKey: env("SENDGRID_API_KEY"),
-        },
-        settings: {
-          defaultFrom: env("SENDGRID_DEFAULT_FROM"),
-          defaultReplyTo: env("SENDGRID_DEFAULT_REPLY_TO"),
-        },
-      },
-      graphql: {
-        endpoint: "/graphql",
-        shadowCRUD: true,
-        playgroundAlways: true,
-        depthLimit: 100,
-        apolloServer: {
-          tracing: false,
-        },
-      },
-
-    }
-  }
-  return {
-    upload: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
+module.exports = ({ env }) => ({
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
     },
-    email: {
-      provider: "sendgrid",
-      providerOptions: {
-        apiKey: env("SENDGRID_API_KEY"),
-      },
-      settings: {
-        defaultFrom: env("SENDGRID_DEFAULT_FROM"),
-        defaultReplyTo: env("SENDGRID_DEFAULT_REPLY_TO"),
-      },
+    actionOptions: {
+      upload: {},
+      delete: {},
     },
-    graphql: {
-      endpoint: "/graphql",
-      shadowCRUD: true,
-      playgroundAlways: true,
-      depthLimit: 100,
-      apolloServer: {
-        tracing: false,
-      },
+  },
+  email: {
+    provider: "sendgrid",
+    providerOptions: {
+      apiKey: env("SENDGRID_API_KEY"),
     },
-  }
-};
+    settings: {
+      defaultFrom: env("SENDGRID_DEFAULT_FROM"),
+      defaultReplyTo: env("SENDGRID_DEFAULT_REPLY_TO"),
+    },
+  },
+  graphql: {
+    endpoint: "/graphql",
+    shadowCRUD: true,
+    playgroundAlways: true,
+    depthLimit: 100,
+    apolloServer: {
+      tracing: false,
+    },
+  },
+});
